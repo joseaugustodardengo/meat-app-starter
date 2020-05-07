0c5db3a8cd5494620289a81b960f30081a4078f0
joseaugustodardengo <joseaugusto.dardengo@gmail.com>
2020-05-06 23:57:58 -0300

    Tratamento de erros com catch
    Criado um arquivo para receber os erros e realizar tratamento e adicionado a função catch no arquivo de serviço dos restaurantes.

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -3,8 +3,10 @@ import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
 import { MEAT_API } from '../app.api'
+import { ErrorHandler } from "../app.error-handler";
 
 @Injectable()
 export class RestaurantsService {
@@ -14,5 +16,6 @@ export class RestaurantsService {
   restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
       .map(response => response.json())
+      .catch(ErrorHandler.handleError)
   }
 }
